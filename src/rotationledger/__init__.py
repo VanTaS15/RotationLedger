6572c153a5f4ff0a4b9aefb0281d64f0ab5ad357
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2018-01-05 15:32:00 +0700

    fix: correct cli

--- src/rotationledger/__init__.py
+++ src/rotationledger/__init__.py
@@ -1,3 +1,6 @@
 """rotationledger: track the lifetime of leaked secrets through git history.
 
 rotationledger reads a committed ``git log -p`` export offline, detects
+credential-shaped strings with Shannon entropy plus named rules, then
+reconstructs each secret's lifetime: where it was introduced, how many days it
+stayed exposed, where it was removed or rotated, and whether it is still present
