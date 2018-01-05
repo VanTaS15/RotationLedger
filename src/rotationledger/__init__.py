a1914f310c6b9db52994e12d79f6bbaa851349fb
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2018-01-05 15:44:00 +0700

    chore: security

--- src/rotationledger/__init__.py
+++ src/rotationledger/__init__.py
@@ -4,3 +4,6 @@ rotationledger reads a committed ``git log -p`` export offline, detects
 credential-shaped strings with Shannon entropy plus named rules, then
 reconstructs each secret's lifetime: where it was introduced, how many days it
 stayed exposed, where it was removed or rotated, and whether it is still present
+at HEAD. The headline artifact is an exposure-days report.
+
+Standard library only, Python 3.11+, no network access.
