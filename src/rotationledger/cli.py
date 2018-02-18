4ab3bb0a764d85de340ef181f25929812f21325a
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2018-02-18 09:32:00 +0700

    perf: improve python

---
+++ src/rotationledger/cli.py
@@ -0,0 +1,9 @@
+"""Command line interface for rotationledger.
+
+Subcommands:
+  scan      list every credential-shaped finding in the export
+  lifetime  reconstruct each secret's introduce/rotate/still-live state
+  report    the exposure-days report, the headline artifact
+  version   print the package version
+
+Exit codes: 0 clean (no findings), 1 findings present, 2 usage error.
