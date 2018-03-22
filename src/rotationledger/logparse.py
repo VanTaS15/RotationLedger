75fe6149fc54cdbbe4bbbb507774098aa87d258a
witthayaevans <309016693+witthayaevans@users.noreply.github.com>
2018-03-22 18:15:00 +0700

    perf: improve security

---
+++ src/rotationledger/logparse.py
@@ -0,0 +1,15 @@
+"""Parse a committed ``git log -p`` export into commits and diff hunks.
+
+This module never invokes git. It reads a text export that a user produced with
+``git log -p --date=iso`` (or similar) and turns it into structured commits.
+Each commit carries its added and removed lines with the file they touched, so
+``lifetime.py`` can reconstruct when a secret entered and left the tree.
+
+The parser is intentionally small and tolerant. It recognises the fields git
+prints by default and ignores anything it does not need, rather than trying to
+model the full diff grammar.
+"""
+
+from __future__ import annotations
+
+import re
