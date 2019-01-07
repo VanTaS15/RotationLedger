66dea61c99194cf5e3c5e6248e029abac19727ec
witthayaevans <309016693+witthayaevans@users.noreply.github.com>
2019-01-07 14:33:00 +0700

    fix: correct rotation

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -2,3 +2,7 @@ import unittest
 
 from rotationledger.detect import scan_line
 
+
+class TestDetect(unittest.TestCase):
+    def test_aws_access_key(self):
+        findings = scan_line('AWS_ACCESS_KEY_ID = "AKIAEXAMPLE00000FAKE"')
