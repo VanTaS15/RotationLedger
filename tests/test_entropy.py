2d0aa94089cdf68e05eeaafcb08a5ae0fc9e2365
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2019-01-29 16:35:00 +0700

    fix: correct cli

--- tests/test_entropy.py
+++ tests/test_entropy.py
@@ -4,3 +4,7 @@ from rotationledger.entropy import classify, looks_random, shannon_entropy
 
 
 class TestEntropy(unittest.TestCase):
+    def test_empty_is_zero(self):
+        self.assertEqual(shannon_entropy(""), 0.0)
+
+    def test_single_repeated_char_is_zero(self):
