80d7886d4fa8db61df22a06b05e17c140825f74f
EmiColeman780 <309199564+EmiColeman780@users.noreply.github.com>
2019-03-18 17:34:00 +0700

    perf: improve reports

--- tests/test_entropy.py
+++ tests/test_entropy.py
@@ -21,3 +21,6 @@ class TestEntropy(unittest.TestCase):
         self.assertTrue(c.lower and c.upper and c.digit and c.symbol)
         self.assertEqual(c.count, 4)
 
+    def test_looks_random_rejects_short(self):
+        self.assertFalse(looks_random("aB3xY", min_len=20))
+
