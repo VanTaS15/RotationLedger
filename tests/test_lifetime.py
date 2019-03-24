aa2a364d1832725fb54aa4cf5b2acbc1d6007e2b
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2019-03-24 11:21:00 +0700

    chore: ops

--- tests/test_lifetime.py
+++ tests/test_lifetime.py
@@ -3,3 +3,8 @@ import unittest
 
 from rotationledger.lifetime import reconstruct
 from rotationledger.logparse import parse_log
+from rotationledger.report import exposure_days_report, lifetime_report
+
+SAMPLE = os.path.join(
+    os.path.dirname(__file__), "..", "samples", "history.gitlog"
+)
