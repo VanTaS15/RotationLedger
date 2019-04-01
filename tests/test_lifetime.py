cbb51cffb3e826e3e01a88e0f501462583af9b7e
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2019-04-01 09:27:00 +0700

    docs: document security

--- tests/test_lifetime.py
+++ tests/test_lifetime.py
@@ -8,3 +8,8 @@ from rotationledger.report import exposure_days_report, lifetime_report
 SAMPLE = os.path.join(
     os.path.dirname(__file__), "..", "samples", "history.gitlog"
 )
+
+
+def load_sample():
+    with open(SAMPLE, "r", encoding="utf-8") as fh:
+        return reconstruct(parse_log(fh.read()))
