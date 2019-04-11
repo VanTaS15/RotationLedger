4745f2f4890ae8a4448df22e39d7278c8c82d1f8
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2019-04-11 15:06:00 +0700

    build: secrets

--- tests/test_logparse.py
+++ tests/test_logparse.py
@@ -4,3 +4,9 @@ from rotationledger.logparse import parse_log
 
 LOG = """commit 1111111222222333333444444555555666666000
 Author: A Dev <a@example.invalid>
+Date:   2026-01-05 09:14:00 +0000
+
+    init commit
+
+diff --git a/config.py b/config.py
+--- /dev/null
